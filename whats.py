7a235e1d4fd93c6891f5e4b2314f2f956cf70f1c
bhumijgupta <bhumijgupta@gmail.com>
2019-06-30 09:25:59 +0530

    Linted with black and flake8

--- whats.py
+++ whats.py
@@ -4,20 +4,21 @@ import time
 import os
 import threading
 import sys
+
 driver = None
 
 
 def main():
     global driver
     try:
-        driver = webdriver.Chrome(os.environ['CHROME_DRIVER'])
+        driver = webdriver.Chrome(os.environ["CHROME_DRIVER"])
     except KeyError:
-        print('Cannot find path to Chrome Driver\n Exiting')
+        print("Cannot find path to Chrome Driver\n Exiting")
         sys.exit(1)
     driver.get("https://web.whatsapp.com/")
-    answer = input('Is the phone connected successfully? (y/Y) -> ')
+    answer = input("Is the phone connected successfully? (y/Y) -> ")
 
-    if str(answer).strip().lower() == 'y':
+    if str(answer).strip().lower() == "y":
         keep_running = True
         while keep_running:
             try:
@@ -25,50 +26,56 @@ def main():
             except:
                 keep_running = False
 
-        print('Thanks for using.....')
+        print("Thanks for using.....")
     else:
-        print('Thanks for using.....')
+        print("Thanks for using.....")
 
 
 def give_options():
     try:
         answer = int(
-            input('1. Send scheduled message\n2. Send quick message\n3. Chat\n-> ').strip())
+            input(
+                "1. Send scheduled message\n2. Send quick message\n3. Chat\n-> "
+            ).strip()
+        )
     except ValueError:
-        print('Invalid Input\nExiting')
+        print("Invalid Input\nExiting")
         sys.exit(1)
 
     if answer == 1:
-        interval = int(input('Enter the interval in seconds -> ').strip())
-        user_name = input('Enter the user -> ').strip()
-        message = input('Enter the message -> ')
+        interval = int(input("Enter the interval in seconds -> ").strip())
+        user_name = input("Enter the user -> ").strip()
+        message = input("Enter the message -> ")
         message_after_interval(interval, user_name, message)
     elif answer == 2:
-        user_name = input('Enter the user -> ').strip()
-        message = input('Enter the message -> ')
+        user_name = input("Enter the user -> ").strip()
+        message = input("Enter the message -> ")
 
-        user_name_list = user_name.split(',')
+        user_name_list = user_name.split(",")
 
         for user_name in user_name_list:
             if user_name:
                 send_message(user_name.strip(), message)
     elif answer == 3:
-        user_name = input('Enter the user -> ').strip()
+        user_name = input("Enter the user -> ").strip()
         initialize_chat(user_name)
     else:
-        print('O.o\nWrong Choice!')
+        print("O.o\nWrong Choice!")
 
 
 def message_after_interval(interval, user_name, message):
-    threading.Timer(interval, send_message, args=[user_name, message, True]).start()
+    threading.Timer(
+        interval, send_message, args=[user_name, message, True]
+    ).start()
 
 
 def initialize_chat(user_name):
     if open_chat(user_name):
-        print('Chat opened')
+        print("Chat opened")
         try:
             all_msgs_text_only = driver.find_element_by_xpath(
-                '//div[contains(@class, "message-text")]')
+                '//div[contains(@class, "message-text")]'
+            )
             print(all_msgs_text_only)
         except Exception as e:
             print(str(e))
@@ -76,34 +83,42 @@ def initialize_chat(user_name):
 
 def open_chat(user_name):
     try:
-        print('Searching for user..... ' + user_name)
-        web_obj = driver.find_element_by_xpath("//input[@title='Search or start new chat']")
+        print("Searching for user..... " + user_name)
+        web_obj = driver.find_element_by_xpath(
+            "//input[@title='Search or start new chat']"
+        )
         web_obj.send_keys(user_name)
         time.sleep(2)
-        elem = driver.find_element_by_xpath('//span[contains(text(),"{0}")]'.format(user_name))
+        elem = driver.find_element_by_xpath(
+            '//span[contains(text(),"{0}")]'.format(user_name)
+        )
         elem.click()
         return True
     except:
-        print('O.o!\nNo user found..')
-        elem = driver.find_element_by_xpath('//button[@class="icon-search-morph"]')
+        print("O.o!\nNo user found..")
+        elem = driver.find_element_by_xpath(
+            '//button[@class="icon-search-morph"]'
+        )
         elem.click()
         return False
 
 
 def send_message(user_name, message, is_interval=False):
     if open_chat(user_name):
-        web_obj = driver.find_element_by_xpath("//div[@contenteditable='true']")
+        web_obj = driver.find_element_by_xpath(
+            "//div[@contenteditable='true']"
+        )
         web_obj.send_keys(message)
 
         if is_interval:
             web_obj.send_keys(Keys.RETURN)
         else:
-            answer = input('Should i send message (y/Y)? -> ')
-            if str(answer).strip().lower() == 'y':
+            answer = input("Should i send message (y/Y)? -> ")
+            if str(answer).strip().lower() == "y":
                 web_obj.send_keys(Keys.RETURN)
             else:
                 web_obj.clear()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
